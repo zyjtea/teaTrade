4721ac9fc54fcde40250180ae81186484113300d
q1120798602 <33975894+q1120798602@users.noreply.github.com>
2018-07-23 10:53:04 +0800

    day3

--- pages/jpzx/jpzx.js
+++ pages/jpzx/jpzx.js
@@ -3,5 +3,24 @@
 
 
 Page({
-  
+  data: {
+    show: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    dengjiData: ['1','2','3'],
+    index: 0//选择的下拉列表下标
+  },
+  // 点击下拉显示框
+  dengjiTap() {
+    console.log(this.setdata)
+    this.setData({
+      show: !this.data.show
+    });
+  },
+
+  biaoqianTap() {
+    console.log(this.data.show)
+    this.setData({
+      show: !this.data.show
+    });
+  },
+ 
 })
