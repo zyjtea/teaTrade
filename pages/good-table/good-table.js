b1db507ac0b3e07471c5ff0cd789c71856b29012
zyjtea <41384709+zyjtea@users.noreply.github.com>
2018-07-24 14:17:44 +0800

    合并
    合并

--- pages/good-table/good-table.js
+++ pages/good-table/good-table.js
@@ -1,15 +1,24 @@
+var gooddata = require('../../utils/goodinfo.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    good: '茶叶1'
-
+    item: []
   },
-  buy() {
-     wx.navigateTo({
-       url: '../goods/goods',
-     })
+  onLoad: function(e) {
+    this.setData({
+      item: gooddata.goodinfo()
+    })
+    console.log(this.data.item)
+  },
+  detail(e) {
+    console.log(e);
+    let id = e.currentTarget.dataset.id;
+    console.log(id);
+    wx.navigateTo({
+      url: '../goods/goods?id='+id
+    })
   }
 })
